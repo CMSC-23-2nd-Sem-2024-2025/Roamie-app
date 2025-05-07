a9b534ded17e9acb41dc5bdd3b4e1c04cb292cdf
clarmndp <cpmandap@up.edu.ph>
2025-05-07 16:26:55 +0800

    feat: added initial navbar

---
+++ lib/components/bottom_nav_bar.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatelessWidget {
+  final int currentIndex;
+  final Function(int) onTap;
+
+  const BottomNavBar({
+    super.key,
+    required this.currentIndex,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: onTap,
+      items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Travel Plans'),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.people),
+          label: 'Find Similar People',
+        ),
+        BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+      ],
+    );
+  }
+}
