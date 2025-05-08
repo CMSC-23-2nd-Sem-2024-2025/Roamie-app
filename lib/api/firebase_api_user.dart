cc30e9c60304a59c20e28eb60f8045bd16d4e3ae
RDCRebugio <rcrebugio@up.edu.ph>
2025-05-08 17:45:26 +0800

    feat: Added user API

---
+++ lib/api/firebase_api_user.dart
@@ -0,0 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FirebaseUserAPI {
+
+  // Create Instance
+  static final FirebaseFirestore db = FirebaseFirestore.instance;
+
+  // Function for adding users
+  Future<String> addUser(Map<String, dynamic> userData) async {
+    try {
+      await db.collection("users").add(userData);
+      return "Successfully added user!";
+    } on FirebaseException catch (e) {
+      return "Failed with error '${e.code}': ${e.message}";
+    }
+   }
+
+  // Get the user info via user id
+  Stream<QuerySnapshot> getUserInfo(String userId) {
+    return db.collection('users').where('userId', isEqualTo: userId).snapshots(); 
+  }
+}
